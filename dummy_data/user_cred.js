cd3a0de657b9cec06f197634f92d17a5ddf276b8
kauramanp <33446281+kauramanp@users.noreply.github.com>
2023-05-16 00:33:30 +0530

    Login work completed

---
+++ dummy_data/user_cred.js
@@ -0,0 +1,18 @@
+const user_creds = [
+    {
+        username: "mohsin",
+        password: "123",
+    },
+    {
+        username: "amanpreet",
+        password: "abc",
+    },
+    {
+        username: "admin",
+        password: "admin",
+    },
+    {
+        username: "manager",
+        password: "manager",
+    }
+]
--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 var loginButton = document.getElementById("loginButton")
 
+var credentials = [];
+
+window.onload = () => {
+    console.log("data " + user_creds)
+    if (user_creds.length != 0) {
+        credentials = user_creds;
+
+    }
+}
+
 loginButton.onclick = function () {
 
-    var username = document.getElementById("email")
-    var password = document.getElementById("password")
+    var username = document.getElementById("email").value
+    var password = document.getElementById("password").value
     console.log("username " + username.value);
     console.log("password " + password.value);
-    if ((username.value == null || username.value === "") || (password.value == null || password.value === "")) {
+    if ((username == null || username === "") || (password == null || password === "")) {
         alert("Enter Email and password to login");
     } else {
+        var isFound = false
+        credentials.forEach(element => {
+            if (element.username == username && element.password == password) {
+                isFound = true
 
+            }
+        });
+
+        if (isFound) {
+            window.location.replace(".../../view_resume/view_resume.html");
+
+        } else {
+            alert("Sorry Credentials are incorrect or not found");
+
+        }
     }
 };
