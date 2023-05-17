91f89989755fb2a310271191520c4367a66b0e6d
kauramanp <33446281+kauramanp@users.noreply.github.com>
2023-05-18 02:41:50 +0530

    working on view resume designs

--- index/index.js
+++ index/index.js
@@ -11,11 +11,8 @@ window.onload = () => {
 }
 
 loginButton.onclick = function () {
-
     var username = document.getElementById("email").value
     var password = document.getElementById("password").value
-    console.log("username " + username.value);
-    console.log("password " + password.value);
     if ((username == null || username === "") || (password == null || password === "")) {
         alert("Enter Email and password to login");
     } else {
@@ -23,13 +20,11 @@ loginButton.onclick = function () {
         credentials.forEach(element => {
             if (element.username == username && element.password == password) {
                 isFound = true
-
             }
         });
 
         if (isFound) {
             window.location.replace("../view_resume/view_resume.html");
-
         } else {
             alert("Sorry Credentials are incorrect or not found");
 
